718e43df1f5f8ed5a00a7ad54dff94182dc28c73
Erica-Ko <68455198+Erica-Ko@users.noreply.github.com>
2020-10-12 16:34:26 +0800

    Update Main.java

--- Class 2 - Assignment(4)/src/com/employee/Main.java
+++ Class 2 - Assignment(4)/src/com/employee/Main.java
@@ -5,6 +5,9 @@ public class Main {
 
     public static void main(String[] args) {
 //    Create two employee objects
+        Employee e1 = new Employer("Hello", 23, "Hong Kong")
+        Employee e2 = new Employer("Kitty", 23, "Hong Kong")
 //    Get the info of the employee
+            e1.getEmployeeInfo()
     }
 }
